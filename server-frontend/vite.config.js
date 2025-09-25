da4d238c611038b003a3e23ea946b1984de05fe8
leeht62 <zdfxd3611@naver.com>
2025-09-25 20:26:45 +0900

    9.25 update

--- server-frontend/vite.config.js
+++ server-frontend/vite.config.js
@@ -1,20 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: env.VITE_API_BASE_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default ({ mode }) => {
+  // .env 파일 로드
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+        }
       }
+    },
+    build: {
+      outDir: 'dist'
     }
-  }
-  ,define: {
-    global: 'window' 
-  }
-})
+  })
+}
