0ef9c0072d36881513a3a2db0c1c135b19cd3bd6
leeht62 <zdfxd3611@naver.com>
2025-09-27 21:26:32 +0900

    today

--- server-frontend/src/lib/api.js
+++ server-frontend/src/lib/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const api = axios.create({
-  baseURL: 'https://today-clothes.shop/api',
+  baseURL: 'http://localhost:8080/api',
   headers: {
     'Content-Type': 'application/json',
   },
