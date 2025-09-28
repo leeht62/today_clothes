a32139f63d705cbbb25042280ba19d66194634ac
leeht62 <zdfxd3611@naver.com>
2025-09-28 20:03:07 +0900

    9.28 change

--- server-frontend/vite.config.js
+++ server-frontend/vite.config.js
@@ -3,24 +3,28 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  // .env 파일 로드
   const env = loadEnv(mode, process.cwd(), '')
+
+  const serverConfig = {
+    host: '0.0.0.0',
+    port: 3000
+  };
+
+  // 개발 모드에서만 proxy 설정 추가
+  if (mode === 'development') {
+    serverConfig.proxy = {
+      '/api': {
+        target: 'https://weather-clothes.store',
+        changeOrigin: true,
+      }
+    };
+  }
 
   return defineConfig({
     plugins: [react()],
-    server: {
-      host: '0.0.0.0',
-      port: 3000,
-      proxy: {
-        '/api': {
-          target: 'https://today-clothes.shop',
-          changeOrigin: true,
-        }
-      }
-    },
+    server: serverConfig,
     build: {
       outDir: 'dist'
     }
-  })
+  });
 }
-
