55e0d6924019926a71abd0bb6bdf22a457b63837
leeht62 <zdfxd3611@naver.com>
2025-09-29 11:36:42 +0900

    9.29 update

--- server-backend/src/main/java/com/server/today_clothes/jwt/JwtAuthenticationFilter.java
+++ server-backend/src/main/java/com/server/today_clothes/jwt/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
@@ -24,12 +25,16 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
     // 1. Request Header에서 JWT 토큰 추출
     String token = resolveToken((HttpServletRequest) request);
+    HttpServletResponse res = (HttpServletResponse) response;
 
     // 2. validateToken으로 토큰 유효성 검사
     if (token != null && jwtTokenProvider.validateToken(token)) {
       String isLogout = (String) redisTemplate.opsForValue().get("logout:" + token);
       if (isLogout != null) {
-        throw new RuntimeException("이미 로그아웃된 토큰입니다.");
+        res.setStatus(HttpServletResponse.SC_FORBIDDEN); // 403
+        res.setContentType("application/json");
+        res.getWriter().write("{\"error\": \"이미 로그아웃된 토큰입니다.\"}");
+        return; // 필터 체인 중단
       }
       // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext에 저장
       Authentication authentication = jwtTokenProvider.getAuthentication(token);
