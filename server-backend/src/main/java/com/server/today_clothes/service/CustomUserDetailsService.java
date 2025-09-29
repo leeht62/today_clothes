9108c63a0b96f689989b7ab7be075dceca331d84
leeht62 <zdfxd3611@naver.com>
2025-09-29 21:44:04 +0900

    9.29 update

--- server-backend/src/main/java/com/server/today_clothes/service/CustomUserDetailsService.java
+++ server-backend/src/main/java/com/server/today_clothes/service/CustomUserDetailsService.java
@@ -2,12 +2,14 @@ package com.server.today_clothes.service;
 
 import com.server.today_clothes.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @RequiredArgsConstructor
@@ -23,9 +25,14 @@ public class CustomUserDetailsService implements UserDetailsService {
 
   // 해당하는 User 의 데이터가 존재한다면 UserDetails 객체로 만들어서 return
   private UserDetails createUserDetails(com.server.today_clothes.VO.User user) {
-    return User.builder()
-        .username(user.getUsername())
-        .password(user.getPassword())
+    return org.springframework.security.core.userdetails.User.builder()
+        .username(user.getUsername()) // Spring Security User의 username
+        .password(user.getPassword()) // 암호화된 비밀번호
+        .authorities(new SimpleGrantedAuthority(user.getRole())) // 권한 설정
+        .accountExpired(false)
+        .accountLocked(false)
+        .credentialsExpired(false)
+        .disabled(false)
         .build();
   }
 }
