9d7ea38150c2c1886e163efa446598b806198a0c
leeht62 <zdfxd3611@naver.com>
2025-10-03 11:36:02 +0900

    10.03 login,fastapi update

--- server-backend/src/main/java/com/server/today_clothes/controller/UserController.java
+++ server-backend/src/main/java/com/server/today_clothes/controller/UserController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -50,6 +51,8 @@ public class UserController {
     } catch (UsernameNotFoundException e) {
       return ResponseEntity.status(HttpStatus.NOT_FOUND)
           .body("사용자를 찾을 수 없습니다.");
+    }catch (BadCredentialsException e) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
   }
   @PostMapping("/logout")
