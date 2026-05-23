6200e2e1bea8baaa03f194c1798557f9683af780
leeht62 <zdfxd3611@naver.com>
2026-05-23 20:25:31 +0900

    feat: payment 및 order 엔티티 추가 #10

--- server-backend/src/main/java/com/server/today_clothes/domain/payment/VO/Payment.java
+++ server-backend/src/main/java/com/server/today_clothes/domain/payment/VO/Payment.java
@@ -1,4 +1,27 @@
 package com.server.today_clothes.domain.payment.VO;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Payment {
+  private Long id;
+  private Long orderId;
+  private Long userId;
+  private String tossOrderId;
+  private String paymentKey;
+  private String idempotencyKey;
+  private Long amount;
+  private PaymentStatus status;
+  private LocalDateTime paidAt;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+
 }
---
+++ server-backend/src/main/java/com/server/today_clothes/domain/payment/VO/PaymentStatus.java
@@ -0,0 +1,9 @@
+package com.server.today_clothes.domain.payment.VO;
+
+public enum PaymentStatus {
+  PENDING,
+  SUCCESS,
+  FAILED,
+  CANCELED,
+  REFUNDED
+}
