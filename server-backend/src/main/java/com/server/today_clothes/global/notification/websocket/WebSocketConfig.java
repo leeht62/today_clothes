f9da3a9a0c4ae9722d2a2596f1e6115a9577fe28
leeht62 <zdfxd3611@naver.com>
2026-07-08 11:58:28 +0900

    fix: cors 오류 해결

--- server-backend/src/main/java/com/server/today_clothes/global/notification/websocket/WebSocketConfig.java
+++ server-backend/src/main/java/com/server/today_clothes/global/notification/websocket/WebSocketConfig.java
@@ -13,9 +13,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/ws") // 클라이언트 연결 URL
-        .setAllowedOrigins("https://today-clothes.shop", "http://today-clothes.shop",
-            "http://www.today-clothes.shop", "https://www.today-clothes.shop",
-            "http://localhost:3000")
+        .setAllowedOriginPatterns(
+            "http://localhost:3000",
+            "https://devcourse-testing.shop",
+            "https://www.devcourse-testing.shop"
+        )
         .withSockJS();
   }
 
